7ff6637a07130d1d9b7e77f6837c12f7f9a9d3f0
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-02-18 15:53:43 +0200

    Enable VNDK build support
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,11 @@ $(call inherit-product, build/target/product/core_64_bit.mk)
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+PRODUCT_USE_VNDK_OVERRIDE := false
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # Add preffered configurations
 PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
