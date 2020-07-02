833ef5dd9d4bab46015140d698763a870308c285
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-07-02 12:38:48 +0300

    health: HACK / Use default health implemntation
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- device.mk
+++ device.mk
@@ -184,8 +184,13 @@ PRODUCT_PACKAGES += \
     modetest \
 
 # Health HAL 2.0
+#PRODUCT_PACKAGES += \
+#    android.hardware.health@2.0-service.xenvm \
+
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service.xenvm \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl \
+    android.hardware.health.storage@1.0-service \
 
 # Keymaster HAL
 # All security related settings are moved into dedicated security.mk
