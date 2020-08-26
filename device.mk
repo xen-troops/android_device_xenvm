58cc53b3b64243c7dc616ea0ebfadb896f62117f
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-08-26 22:27:51 +0300

    xenvm: Enable AVB
    Enable android verified boot 2.0
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ AB_OTA_PARTITIONS := \
     boot \
     system \
     vendor \
+    vbmeta \
 
 PRODUCT_PACKAGES += \
     update_verifier \
@@ -118,15 +119,17 @@ PRODUCT_COPY_FILES +=\
 ifeq ($(ENABLE_AVB),true)
   PRODUCT_COPY_FILES += \
     device/xen/xenvm/fstab.xenvm.avb:root/fstab.xenvm \
+    device/xen/xenvm/fstab.xenvm.avb:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.xenvm \
     device/xen/xenvm/fstab.xenvm.avb:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.xenvm \
 
 TARGET_RECOVERY_FSTAB := device/xen/xenvm/fstab.xenvm.avb
 else
   PRODUCT_COPY_FILES += \
     device/xen/xenvm/fstab.xenvm:root/fstab.xenvm \
-    device/xen/xenvm/fstab.recovery.xenvm:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.xenvm \
+    device/xen/xenvm/fstab.xenvm:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.xenvm \
+    device/xen/xenvm/fstab.xenvm:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.xenvm \
 
-TARGET_RECOVERY_FSTAB := device/xen/xenvm/fstab.recovery.xenvm
+TARGET_RECOVERY_FSTAB := device/xen/xenvm/fstab.xenvm
 endif
 
 PRODUCT_COPY_FILES += \
