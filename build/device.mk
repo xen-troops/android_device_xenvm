5831a1f68d3ac3d4e59bf0b009affa85b66b5156
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2021-07-07 17:37:38 +0300

    Unify build variables by prefix XT_
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- build/device.mk
+++ build/device.mk
@@ -327,7 +327,7 @@ $(call inherit-product, \
 $(call inherit-product, device/xen/xenvm/build/graphics.mk)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
-ifeq ($(USE_G_APPS),true)
+ifeq ($(XT_USE_GOOGLE_APPS),true)
 $(call inherit-product-if-exists, vendor/Google/Google-product.mk)
 endif
 $(call inherit-product, device/xen/xenvm/build/security.mk)
