2e03b39a26dbe5d2a1840f19495e5faa3f6f21fc
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-01-22 11:34:29 +0000

    xenvm: use vendor properties
    Due to the absence of a system ext partition, we will utilize vendor
    properties.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- build/device.mk
+++ build/device.mk
@@ -343,7 +343,7 @@ DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/osdouble_overlay
 PRODUCT_COPY_FILES += \
     device/generic/car/emulator/cluster/display_settings.xml:system/etc/display_settings.xml
 
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
+PRODUCT_VENDOR_PROPERTIES += \
     ro.setupwizard.mode?=OPTIONAL
 
 
