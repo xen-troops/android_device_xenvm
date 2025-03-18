2145bbb81e1a9b99961687d879533c242cc7ee6a
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2025-03-18 14:28:52 +0200

    Added overriden impl. of the audio.primary.caremu
    Current board doesn't have any mixers(audio as well), and original implementation
    crashes on mixer print. Fix crash just to be able to boot.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Reviewed-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>

--- build/device.mk
+++ build/device.mk
@@ -399,13 +399,18 @@ PRODUCT_PACKAGES += \
     updatable-media
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.audio.primary=caremu \
+    ro.hardware.audio.primary=caremu-ext \
     ro.vendor.caremu.audiohal.out_period_ms=16 \
     ro.vendor.caremu.audiohal.in_period_ms=16
 
 # Car Emulator Audio HAL
 PRODUCT_PACKAGES += \
     audio.primary.caremu \
+    audio.r_submix.default \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
+    audio.primary.caremu-ext \
     audio.r_submix.default \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
